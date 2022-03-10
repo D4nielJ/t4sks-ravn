7d0667b2dbdf7399b9ce6d2490fcf69674d107c1
d4nielj <d4niel.djm@gmail.com>
2022-03-09 23:01:56 -0500

    Editing Link from Navbar component

--- components/navbar/Link.js
+++ components/navbar/Link.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import NextLink from 'next/link';
+import { Heading } from '../shared';
 
 const Link = ({ icon, text, to }) => {
   return (
     <NextLink href={to}>
-      <a>{text}</a>
+      <a className="flex items-center gap-x-3 border-r py-2 pr-8">
+        <span>{icon}</span>
+        <Heading type="DisplayMedium">{text}</Heading>
+      </a>
     </NextLink>
   );
 };
--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,34 +1,63 @@
 import React from 'react';
+import {
+  MdOutlineDashboard,
+  MdOutlineMenu,
+  MdCalendarToday,
+  MdOutlineAvTimer,
+  MdOutlinePieChart,
+  MdOutlineSettings,
+} from 'react-icons/md';
+import { FiBriefcase } from 'react-icons/fi';
 import { Logo } from '../shared';
 import Link from './Link';
 
 const Links = [
   {
-    icon: <></>,
-    text: 'DASHBOARD',
+    icon: <MdOutlineDashboard />,
+    text: 'Dashboard',
     to: '/',
   },
   {
-    icon: <></>,
-    text: 'PROJECTS',
+    icon: <FiBriefcase />,
+    text: 'Projects',
     to: '/projects',
   },
   {
-    icon: <></>,
-    text: 'MY TASK',
+    icon: <MdOutlineMenu />,
+    text: 'My task',
     to: '/mytask',
+  },
+  {
+    icon: <MdCalendarToday />,
+    text: 'Calendar',
+    to: '/calendar',
+  },
+  {
+    icon: <MdOutlineAvTimer />,
+    text: 'Time manage',
+    to: '/time',
+  },
+  {
+    icon: <MdOutlinePieChart />,
+    text: 'Reports',
+    to: '/reports',
+  },
+  {
+    icon: <MdOutlineSettings />,
+    text: 'Settings',
+    to: '/settings',
   },
 ];
 
 const Navbar = () => {
   return (
-    <div className="flex min-w-[12rem] flex-col items-center rounded-2xl bg-neutral-dark pt-6">
+    <div className="flex flex-col items-center rounded-2xl bg-neutral-dark pt-6">
       <Logo />
       <nav>
-        <ul>
+        <ul className="flex flex-col gap-y-4 pt-8">
           {Links.map(({ icon, text, to }) => (
-            <li key={text}>
-              <Link to={to} text={text} />
+            <li key={text} className="pl-4">
+              <Link text={text} to={text} icon={icon} />
             </li>
           ))}
         </ul>
