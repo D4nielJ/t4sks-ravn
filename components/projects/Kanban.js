42f36cf0ddb85b9c25a97f853bb354fb08180c80
d4nielj <d4niel.djm@gmail.com>
2022-03-11 15:32:22 -0500

    Retrieve data of tasks from server

--- components/projects/Kanban.js
+++ components/projects/Kanban.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import SimpleBar from 'simplebar-react';
+import { useQuery } from 'react-query';
 import ConfigButtons from './ConfigButtons';
 import KanbanColumn from './KanbanColumn';
+import { useGetTasks } from '../../lib/apiClient';
 
 const workingTasks = [
   {
@@ -60,12 +62,19 @@ const inProgressTasks = [
   },
 ];
 
-const getTasks = () => {
-  const res = fetch()
-}
-
 const Kanban = () => {
-  const tasksQuery = useQuery('tasks', getTodos)
+  const { isLoading, error, data } = useGetTasks();
+  console.log('🚀 ~ file: Kanban.js ~ line 67 ~ Kanban ~ data', data);
+  console.log('🚀 ~ file: Kanban.js ~ line 67 ~ Kanban ~ isLoading', isLoading);
+  console.log('🚀 ~ file: Kanban.js ~ line 67 ~ Kanban ~ error', error);
+
+  if (isLoading) {
+    return <div>Is loading</div>;
+  }
+
+  if (error) {
+    return <div>{error.response.message}</div>;
+  }
 
   return (
     <div className="flex flex-col gap-8">
